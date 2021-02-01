b25e7b5daddba0cd10b1ba00d558a0cb9be3c4e6
Oliver Reardon-Smith <oliverreardonsmith@gmail.com>
2021-02-01 14:59:25 +0100

    rename module

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 import numpy
 
-setup(name = 'cPSCS', version = '1.0',  \
+setup(name = 'clifford_t_estim', version = '1.0',  \
    ext_modules = [Extension('clifford_t_estim', ['cmodule.c', 'AG.c', "QCircuit.c"], extra_compile_args=['-g'],include_dirs=[numpy.get_include()], libraries=["gsl", "gslcblas"])])
